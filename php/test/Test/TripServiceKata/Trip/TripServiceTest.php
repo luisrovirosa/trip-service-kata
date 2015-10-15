70a3a6608522226c81266d5e345540eb87ce941a
Luis Rovirosa <luisrovirosa@gmail.com>
2015-10-15 18:17:25 +0200

    Test refactoring

--- php/test/Test/TripServiceKata/Trip/TripServiceTest.php
+++ php/test/Test/TripServiceKata/Trip/TripServiceTest.php
@@ -10,40 +10,89 @@ class TripServiceTest extends PHPUnit_Framework_TestCase
 {
     const UNUSED_USER = null;
     const GUEST_USER = null;
+    private $nonFriend;
+    private $friend;
+
+    /** @var  User */
+    private $loggedUser;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->loggedUser = new User('Luis');
+        $this->nonFriend = new User('Luis');
+        $this->nonFriend->addTrip(new Trip('Secret trip'));
+        $this->friend = new User('Luis');
+        $this->friend->addFriend($this->loggedUser());
+        $this->friend->addTrip(new Trip('Barcelona'));
+    }
 
     /** @test */
     public function should_throw_an_exception_when_the_user_is_not_logged()
     {
         $this->setExpectedException('TripServiceKata\Exception\UserNotLoggedInException');
         $tripService = new TestableTripService(self::GUEST_USER);
-        $luis = new User('Luis');
-        $tripService->getTripsByUser($luis);
+        $tripService->getTripsByUser($this->anyUser());
     }
 
     /** @test */
     public function should_retrieve_an_empty_list_when_users_are_not_friends()
     {
-        $luis = new User('Luis');
-        $loggedUser = new User('Concha');
-        $tripService = new TestableTripService($loggedUser);
-
-        $trips = $tripService->getTripsByUser($luis);
-
-        $this->assertEquals([], $trips);
+        $this->assertEquals([], $this->trips($this->nonFriend()));
     }
 
     /** @test */
     public function should_retrieve_the_lists_of_trips_of_a_friend()
     {
-        $luis = new User('Luis');
-        $loggedUser = new User('Concha');
-        $luis->addFriend($loggedUser);
-        $trip = new Trip();
-        $luis->addTrip($trip);
-        $tripService = new TestableTripService($loggedUser);
+        $friend = $this->friend();
+        $this->assertEquals($friend->getTrips(), $this->trips($friend));
+    }
 
-        $trips = $tripService->getTripsByUser($luis);
+    // Helpers
 
-        $this->assertEquals([$trip], $trips);
+    /**
+     * @return User
+     */
+    private function anyUser()
+    {
+        return new User('John Doe');
+    }
+
+    /**
+     * @return User
+     */
+    private function loggedUser()
+    {
+        return $this->loggedUser;
+    }
+
+    /**
+     * @return User
+     */
+    private function nonFriend()
+    {
+        return $this->nonFriend;
+    }
+
+    /**
+     * @return User
+     */
+    private function friend()
+    {
+        return $this->friend;
+    }
+
+    /**
+     * @param $user
+     * @return array|\TripServiceKata\Trip\Trip[]
+     * @throws \TripServiceKata\Exception\UserNotLoggedInException
+     */
+    private function trips($user)
+    {
+        $tripService = new TestableTripService($this->loggedUser());
+
+        $trips = $tripService->getTripsByUser($user);
+
+        return $trips;
     }
 }
